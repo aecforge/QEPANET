32059a2066e4ae217a968009c20d2ea6f97927b2
albertodeluca <none@none>
2016-07-05 10:37:55 +0200

    Move node tool: added geometry update logic.

--- tools/move_node_tool.py
+++ tools/move_node_tool.py
@@ -2,7 +2,8 @@
 from PyQt4.QtCore import Qt, QTimer
 from PyQt4.QtGui import QCursor, QColor
 from qgis.gui import QgsMapTool, QgsVertexMarker, QgsRubberBand, QgsMapCanvasSnapper
-from qgis.core import QgsPoint, QgsRaster, QgsVectorLayer, QgsProject, QgsSnapper, QgsTolerance, QGis, QgsFeatureRequest, QgsFeature, QgsGeometry
+from qgis.core import QgsPoint, QgsRaster, QgsVectorLayer, QgsProject, QgsSnapper, QgsTolerance, QGis,\
+    QgsFeatureRequest, QgsFeature, QgsGeometry, QgsVectorLayerEditUtils, QgsVectorDataProvider
 import qgis
 from ..geo_utils import utils as geo_utils
 from ..geo_utils import vector_utils as vutils
@@ -24,13 +25,16 @@ class MoveNodeTool(QgsMapTool):
         self.pipes_vlay_id = geo_utils.LayerUtils.get_lay_id('pipes')  # TODO: softcode
         self.pipes_vlay = geo_utils.LayerUtils.get_lay_from_id(self.pipes_vlay_id)
 
-        self.outlet_marker = QgsVertexMarker(self.canvas())
-
         self.vertex_marker = QgsVertexMarker(self.canvas())
         self.mouse_clicked = False
         self.snapped_feat_id = None
+        self.selected_node_ft = None
+        self.mouse_pt = None
 
         self.rubber_bands_d = {}
+
+        self.adj_pipes_fts_d = {}
+
 
     def canvasPressEvent(self, event):
 
@@ -39,32 +43,35 @@ class MoveNodeTool(QgsMapTool):
 
         if event.button() == Qt.RightButton:
             self.mouse_clicked = False
+
         if event.button() == Qt.LeftButton:
             self.mouse_clicked = True
             request = QgsFeatureRequest().setFilterFid(self.snapped_feat_id)
             list = [feat for feat in self.nodes_vlay.getFeatures(request)]
-            selected_node_ft = QgsFeature(list[0])
-            selected_node_ft_eid = selected_node_ft.attribute('id')  # TODO: softcode
+            self.selected_node_ft = QgsFeature(list[0])
+
+            selected_node_ft_eid = self.selected_node_ft.attribute('id')  # TODO: softcode
 
             # Find links that start or end in selected point
-            expression = u'"start_node" = \'' + selected_node_ft_eid + '\' or "end_node" = \'' + selected_node_ft_eid + '\''
+            expression = u'"start_node" = \'' + selected_node_ft_eid + '\' or "end_node" = \'' + selected_node_ft_eid + '\'' # TODO: softcode
             request = QgsFeatureRequest().setFilterExpression(expression)
             adjacent_pipes_fts = self.pipes_vlay.getFeatures(request)
 
-            rb_index = 0;
+            rb_index = 0
             for adjacent_pipes_ft in adjacent_pipes_fts:
-                closest = adjacent_pipes_ft.geometry().closestVertex(selected_node_ft.geometry().asPoint())
+                closest = adjacent_pipes_ft.geometry().closestVertex(self.selected_node_ft.geometry().asPoint())
 
-                next_vertext_id = -1
                 if closest[1] == 0:
                     next_vertext_id = closest[1] + 1
                 else:
                     next_vertext_id = closest[1] - 1
 
+                self.adj_pipes_fts_d[adjacent_pipes_ft] = closest[1]
+
                 rubber_band_start_pt = adjacent_pipes_ft.geometry().vertexAt(next_vertext_id)
 
                 self.rubber_bands_d[rb_index] = QgsRubberBand(self.canvas(), False)  # False = not a polygon
-                points = [rubber_band_start_pt, selected_node_ft.geometry().asPoint()]
+                points = [rubber_band_start_pt, self.selected_node_ft.geometry().asPoint()]
                 self.rubber_bands_d[rb_index].setToGeometry(QgsGeometry.fromPolyline(points), None)
                 self.rubber_bands_d[rb_index].setColor(QColor(255, 128, 128))
                 self.rubber_bands_d[rb_index].setWidth(1)
@@ -73,6 +80,8 @@ class MoveNodeTool(QgsMapTool):
                 rb_index += 1
 
     def canvasMoveEvent(self, event):
+
+        self.mouse_pt = self.toMapCoordinates(event.pos())
 
         if not self.mouse_clicked:
             # Mouse not clicked: snapping to closest vertex
@@ -94,15 +103,14 @@ class MoveNodeTool(QgsMapTool):
             else:
                 self.snapped_feat_id = None
                 self.vertex_marker.hide()
+                self.selected_node_ft = None
 
         else:
             if self.snapped_feat_id is not None:
                 # Mouse clicked and vertex selected
-                pt = self.toMapCoordinates(event.pos())
-
                 # Update rubber bands
                 for key, value in self.rubber_bands_d.iteritems():
-                    self.rubber_bands_d[key].movePoint(1, pt)
+                    self.rubber_bands_d[key].movePoint(1, self.mouse_pt)
 
     def canvasReleaseEvent(self, event):
 
@@ -115,6 +123,37 @@ class MoveNodeTool(QgsMapTool):
             for key in self.rubber_bands_d.keys():
                 self.iface.mapCanvas().scene().removeItem(self.rubber_bands_d[key])
                 del self.rubber_bands_d[key]
+
+            if self.selected_node_ft.id() is not None:
+
+                # Update node geometry
+                self.nodes_vlay.beginEditCommand("Update node geometry")
+                caps = self.nodes_vlay.dataProvider().capabilities()
+                if caps & QgsVectorDataProvider.ChangeGeometries:
+                    edit_utils = QgsVectorLayerEditUtils(self.nodes_vlay)
+                    edit_utils.moveVertex(
+                            self.mouse_pt.x(),
+                            self.mouse_pt.y(),
+                            self.selected_node_ft.id(),
+                            0)
+                    self.refresh_layer(self.nodes_vlay)
+                self.nodes_vlay.endEditCommand()
+
+                # Update links geometries
+                self.pipes_vlay.beginEditCommand("Update pipes geometry")
+                caps = self.pipes_vlay.dataProvider().capabilities()
+                if caps & QgsVectorDataProvider.ChangeGeometries:
+                    for feat, vertex_index in self.adj_pipes_fts_d.iteritems():
+                        edit_utils = QgsVectorLayerEditUtils(self.pipes_vlay)
+                        edit_utils.moveVertex(
+                                self.mouse_pt.x(),
+                                self.mouse_pt.y(),
+                                feat.id(),
+                                vertex_index)
+
+                    self.refresh_layer(self.pipes_vlay)
+                self.pipes_vlay.endEditCommand()
+                self.adj_pipes_fts_d.clear()
 
     def activate(self):
 
@@ -129,7 +168,6 @@ class MoveNodeTool(QgsMapTool):
                                                       100,
                                                       True)
 
-
     def deactivate(self):
         pass
 
@@ -142,6 +180,8 @@ class MoveNodeTool(QgsMapTool):
     def isEditTool(self):
         return True
 
-    def reset_marker(self):
-        self.outlet_marker.hide()
-        self.canvas().scene().removeItem(self.outlet_marker)
+    def refresh_layer(self, layer):
+        if self.iface.mapCanvas().isCachingEnabled():
+            layer.setCacheImage(None)
+        else:
+            self.iface.mapCanvas().refresh()
