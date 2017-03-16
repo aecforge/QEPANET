4fc5a2f098ba5070d98bc1419eba560567208db0
Alberto <albertodeluca3@gmail.com>
2017-03-16 09:55:06 +0100

    - Bug fixed: error when Cancel pressed in "Save as..." dialog.

--- ui/qepanet_dockwidget.py
+++ ui/qepanet_dockwidget.py
@@ -884,7 +884,7 @@ class QEpanetDockWidget(QtGui.QDockWidget, FORM_CLASS):
         inp_file_path = QFileDialog.getSaveFileName(
             self, 'Save project as...', self.params.last_project_dir, 'INP files (*.inp)')
 
-        if inp_file_path is not None:
+        if inp_file_path is not None and inp_file_path:
             self.inp_file_path = inp_file_path
             self.txt_prj_file.setText(self.inp_file_path)
 
