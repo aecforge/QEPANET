06dee13368c7e9c56cf15930f8537bf92a36d630
Alberto <albertodeluca3@gmail.com>
2017-08-10 15:13:24 +0200

    - Version up to 0.9

--- tools/parameters.py
+++ tools/parameters.py
@@ -14,7 +14,7 @@ from ..model.water_quality import Reactions
 
 class Parameters(Observable):
 
-    plug_in_name = 'QEPANET 0.42'
+    plug_in_name = 'QEPANET 0.9'
     config_file_name = 'config.ini'
 
     path = os.path.dirname(os.path.realpath(__file__))
